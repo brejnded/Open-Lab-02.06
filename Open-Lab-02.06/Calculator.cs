0c6c7560d75a993e4cda9a98f727a58aac0655a5
Matúš Nekoranec <113455013+brejnded@users.noreply.github.com>
2022-09-22 08:23:12 +0200

    Update Calculator.cs

--- Open-Lab-02.06/Calculator.cs
+++ Open-Lab-02.06/Calculator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_02._06
 {
@@ -6,6 +6,14 @@ namespace Open_Lab_02._06
     {
         public bool Divisible(int number)
         {
+            if (number % 100 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
             throw new NotImplementedException();
         }
     }
